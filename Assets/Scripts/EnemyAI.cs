c5d9621d3d4277955ba6f34c1c6cc736e5520f1d
HyunsangJoo <joohs86@naver.com>
2021-11-05 22:57:15 +0900

    Update 11_05_2nd
    10:56pm
    Update - enemyAI chase range

--- Assets/Scripts/EnemyAI.cs
+++ Assets/Scripts/EnemyAI.cs
@@ -6,7 +6,10 @@ using UnityEngine.AI;
 public class EnemyAI : MonoBehaviour
 {
     [SerializeField] Transform target;
+    [SerializeField] float chaseRange = 5f;
+    
     NavMeshAgent navMeshAgent;
+    float distanceToTarget = Mathf.Infinity;
 
     void Start()
     {
@@ -17,7 +20,12 @@ public class EnemyAI : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        navMeshAgent.SetDestination(target.position);
+        distanceToTarget = Vector3.Distance(target.position, transform.position);
+
+        if (distanceToTarget <= chaseRange)
+        {
+            navMeshAgent.SetDestination(target.position);
+        }
         
     }
 
